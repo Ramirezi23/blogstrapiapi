9ea3974a3f058d92f5f5ac7e89dbb1290ee578b5
Nelson Nabais <nelsonnabais@gmail.com>
2024-02-12 12:08:39 +0000

    sexto commit

--- config/admin.js
+++ config/admin.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', ['dJ7+F86LIbjFqI1MH0YD2A==']),
     },
   },
   flags: {

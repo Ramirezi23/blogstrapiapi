f7e9a2eb1b729fd47d20e886cd7b5d768010f8a8
Nelson Nabais <nelsonnabais@gmail.com>
2024-02-19 10:08:48 +0000

    alterar config/admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', ['mr07V4yGLITzv/zWEGa86Q==']),
+    secret: env('ADMIN_JWT_SECRET', 'mr07V4yGLITzv/zWEGa86Q=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', ['QAGXintCUI85JXQ9abAYdw==']),
+    salt: env('API_TOKEN_SALT', 'QAGXintCUI85JXQ9abAYdw=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', ['dJ7+F86LIbjFqI1MH0YD2A==']),
+      salt: env('TRANSFER_TOKEN_SALT', 'dJ7+F86LIbjFqI1MH0YD2A=='),
     },
   },
   flags: {

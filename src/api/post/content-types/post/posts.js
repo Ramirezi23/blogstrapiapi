bccac09d111cf78ac88a174a049c165912fe9b72
Nelson Nabais <nelsonnabais@gmail.com>
2024-02-19 09:55:03 +0000

    adicionei lifecycle hooks

--- src/api/post/content-types/post/posts.js
+++ src/api/post/content-types/post/posts.js
@@ -8,7 +8,7 @@ module.exports = {
       axios.post('https://api.netlify.com/build_hooks/65ce24ee5e61fe16aea8099a')
     },
 
-    async afterUpdate(result, params data) {
+    async afterUpdate(result, params, data) {
       axios.post('https://api.netlify.com/build_hooks/65ce24ee5e61fe16aea8099a')
     },
   }
